daa66396fd675e0187cb10fde233c2261ebe231a
FMishka <roytman02@mail.ru>
2024-01-12 19:17:55 +0300

    connection db

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.sql.Connection;
 public class Main {
     public static void main(String[] args) {
+        DbFunctions db=new DbFunctions();
+        // db connection using postgres
+        Connection conn=db.connect_to_db("TravelAgency","postgres","1");
+        db.createTable(conn,"employee");
+        db.insert_row(conn,"employee","GK","Voyager-1");
+        //db.update_name(conn,"employee","GK","RGK");
+        //db.search_by_name(conn,"employee","RGK");
+        //db.delete_row_by_name(conn,"employee","RGK");
+        //db.delete_row_by_id(conn,"employee",4);
+        //db.read_data(conn,"employee");
+        //db.delete_table(conn,"employee");
 
     }
 }

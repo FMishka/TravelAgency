33c2ed8c126ed5842fd4f6dadf5a8dc9b4799afb
FMishka <roytman02@mail.ru>
2024-01-13 22:14:23 +0300

    edited insert function

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ import java.sql.Connection;
 import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
-//        DbFunctions db=new DbFunctions();
+        DbFunctions db=new DbFunctions();
         // db connection using postgres
-//        Connection conn=db.connect_to_db("TravelAgency","postgres","admin");
+        Connection conn=db.connection("TravelAgency","postgres","admin");
 //        db.createTable(conn,"employee");
 //        db.insert_row(conn,"employee","GK","Voyager-1");
         //db.update_name(conn,"employee","GK","RGK");
@@ -16,6 +16,27 @@ public class Main {
         //db.delete_row_by_id(conn,"employee",4);
         //db.read_data(conn,"employee");
         //db.delete_table(conn,"employee");
+
+        String password = new String(Files.readAllBytes(Paths.get("password.txt")), StandardCharsets.UTF_8);
+        conn = db.connection("TravelAgency","postgres",password);
+
+        String[] fields = new String[]{
+                "user_id",
+                "login",
+                "userpassword",
+                "isadmin",
+                "passport"
+        };
+
+        String[] values = new String[]{
+                "2",
+                "'login'",
+                "123",
+                "false",
+                "123456789"
+        };
+
+        db.insertRow(conn, "users", values);
 
         Model model = new Model();
 
